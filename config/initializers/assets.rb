74e8dc5b180bf5c8cd71a666598b76549d5f1532
Esteban Zignego <esteban.zignego@gmail.com>
2016-01-06 09:05:01 -0300

    add home template

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,4 +5,16 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
+#CSS
+Rails.application.config.assets.precompile += %w( bootstrap.min.css )
+Rails.application.config.assets.precompile += %w( font-awesome/css/font-awesome.min.css )
+Rails.application.config.assets.precompile += %w( animate.min.css )
+Rails.application.config.assets.precompile += %w( creative.css )
+
+#JAVASCRIPT
+Rails.application.config.assets.precompile += %w( jquery.js )
+Rails.application.config.assets.precompile += %w( bootstrap.min.js )
+Rails.application.config.assets.precompile += %w( jquery.easing.min.js )
+Rails.application.config.assets.precompile += %w( jquery.fittext.js )
+Rails.application.config.assets.precompile += %w( wow.min.js )
+Rails.application.config.assets.precompile += %w( creative.js )
